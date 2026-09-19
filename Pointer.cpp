c01dc74e477abc696628f4c7498079da2caf1419
Harsh Panchal <harsh220807@gmail.com>
2026-09-19 19:13:12 +0530

    Delete Pointer.cpp

--- Pointer.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std ;
-int main(){
-    int x = 7 ;
-    int* ptr = &x ;          // int* ko ek alag hi datatype maan kr aage smjhne me aasani hogi.
-    cout << &x << endl ;
-    cout << ptr << endl ;
-    cout << &ptr << endl ;
-}
